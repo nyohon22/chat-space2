ad0aa22c87dd97d73395d4f6bafdb5dcb6d008d5
nyohon22 <kimurapun22@gmail.com>
2019-06-13 19:45:31 +0900

    0613 ver2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ before_action :set_group, only: [:edit,:update]
 
   def create
     @group = Group.new(group_params)
-    # binding.pry
+
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
