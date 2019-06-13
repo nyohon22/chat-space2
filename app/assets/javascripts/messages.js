28a21bd9ffa09d1739ecaef46cd52ab4797ce04d
nyohon22 <kimurapun22@gmail.com>
2019-06-13 19:18:45 +0900

    0613

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,14 +1,58 @@
 $(function(){
+  function buildHTML(message){
+    var content = message.content ? `${ message.comment }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__detail">
+                    <p class="message__detail__current-user-name">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__detail__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    var formData = formData(this);
+    // console.log("aaa")
+    var message = new FormData(this);
     var url = $(this).attr('action')
-    $.ajax{
+    // console.log(url)
+    $.ajax({
       url: url,
-      type: "POST"
-      data: formData,
-      dataType: 'json'
-      process
-    }
+      type: "POST",
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html);
+      $('.textbox').val('');
+      function scrollBottom(){
+        var target = $('.message').last();
+        var position = target.offset().top + $('.messages').scrollTop();
+        $('.messages').animate({
+          scrollTop: position
+        }, 300, 'swing');
+      }
+    })
+    .fail(function(data){
+      alert('メッセージの送信が出来ませんでした');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);
+    })
+
   })
 });
